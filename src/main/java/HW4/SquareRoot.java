d87040492783017a018e432849e942fca679a556
alina <alina.starzhnik@gmail.com>
2021-11-11 12:37:28 +0100

    HW4

---
+++ src/main/java/HW4/SquareRoot.java
@@ -0,0 +1,47 @@
+package HW4;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+
+
+    private static final double ACCURACY = 0.01;
+
+    public static void main(String[] args) {
+
+        calculateRoot();
+
+    }
+
+
+    public static void calculateRoot() {
+        boolean isValid = false;
+        double value = 0;
+        while (!isValid) {
+            System.out.println("Please enter a value");
+            value = new Scanner(System.in).nextDouble();
+            if (value >= 0) {
+                isValid = true;
+            } else {
+                System.out.println("Неверное значение");
+            }
+        }
+
+        if (value == 0 || value == 1) {
+            System.out.println("Квадратный корень числа " + value + " равен " + value);
+        } else {
+            for (double i = 1; i < value; i += 0.01) {
+                if (value - i * i <= ACCURACY) {
+                    double result = (double) Math.round(i * 1000) / 1000;
+                    System.out.println("Квадратный корень числа " + value + " равен " + result);
+                    System.out.println("Проверка: " + result + " * " + result + " = " + result * result +
+                            ", погрешность: " + (value - result * result));
+                    return;
+
+                }
+
+            }
+
+        }
+    }
+}
