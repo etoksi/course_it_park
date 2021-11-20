d4df1fe8f4796b4da42afc559e2bdf9d609a1b0a
alina <alina.starzhnik@gmail.com>
2021-11-20 11:56:35 +0100

    HW6

--- src/main/java/HW6/ReadInput.java
+++ src/main/java/HW6/ReadInput.java
@@ -10,6 +10,13 @@ public class ReadInput {
             scanner.next();
             System.out.println("Wrong input. Please, try again!");
         }
-        return scanner.nextInt();
+        int input = scanner.nextInt();
+        if(input >= -1){
+            return input;
+        }
+        else {
+            System.out.println("Please enter a value starting from -1");
+            return readInteger();
+        }
     }
 }
