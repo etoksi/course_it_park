2d2472670718aeac55b69603040f794ebc63f5c5
alina <alina.starzhnik@gmail.com>
2021-11-23 20:27:09 +0100

    HW6

--- src/main/java/HW6/Fibonacci.java
+++ src/main/java/HW6/Fibonacci.java
@@ -3,31 +3,29 @@ package HW6;
 import java.util.Arrays;
 
 public class Fibonacci {
-    private int[] arrayOfFibonacciNumbers = new int[1];
-    boolean flag = true;
+    private int[] arrayOfFibonacciNumbers = {0, 1};
 
-    public int returnFibonacci(int index){
-        if (arrayOfFibonacciNumbers.length <= index){
+    public int returnFibonacci(int index) {
+        if (arrayOfFibonacciNumbers.length <= index) {
             arrayOfFibonacciNumbers = Arrays.copyOf(arrayOfFibonacciNumbers, index);
         }
-        if(arrayOfFibonacciNumbers[index-1] != 0 || (arrayOfFibonacciNumbers[index-1] == 0 && index <= 2)){
+        if (arrayOfFibonacciNumbers[index - 1] != 0 || index == 1) {
             System.out.println("Used value from array.");
             System.out.println(Arrays.toString(arrayOfFibonacciNumbers));
-            return arrayOfFibonacciNumbers[index-1];
+            return arrayOfFibonacciNumbers[index - 1];
         }
-        arrayOfFibonacciNumbers[index-1] = calculateFibonacciNumber(index);
+        arrayOfFibonacciNumbers[index - 1] = calculateFibonacciNumber(index);
         System.out.println("Calculated recursive. Array was modified: ");
         System.out.println(Arrays.toString(arrayOfFibonacciNumbers));
-        return arrayOfFibonacciNumbers[index-1];
+        return arrayOfFibonacciNumbers[index - 1];
     }
 
     private int calculateFibonacciNumber(int index) {
-        if (index == 1){
+        if (index == 1) {
             return 0;
-        }
-        else if(index == 2){
+        } else if (index == 2) {
             return 1;
         }
-        return calculateFibonacciNumber(index-1) + calculateFibonacciNumber(index-2);
+        return calculateFibonacciNumber(index - 1) + calculateFibonacciNumber(index - 2);
     }
 }
