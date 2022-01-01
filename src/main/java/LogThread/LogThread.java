6c7600a7ac8ad3f1d0516c75e00af55acd2ea82b
alina <alina.starzhnik@gmail.com>
2022-01-01 20:32:28 +0100

    HW16

--- src/main/java/LogThread/LogThread.java
+++ src/main/java/LogThread/LogThread.java
@@ -29,6 +29,7 @@ class LogThread implements Runnable {
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 
